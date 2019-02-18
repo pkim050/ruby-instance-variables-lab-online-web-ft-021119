c3a8cb7ae2241ae3741b015d78f5cdfcda5cb2c2
Patrick Kim <pkim050@gmail.com>
2019-02-18 21:45:43 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  def name=(dog_name) #writer method
+    @this_dogs_name = dog_name
+  end
+  
+  def name #reader method
+    @this_dogs_name
+  end
+end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
